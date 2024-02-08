2cb934f6f65426eaa08c47ca753650a8e7613a1f
= <=>
2024-02-08 23:36:07 +0530

    input field tracking and handling added.

--- App.tsx
+++ App.tsx
@@ -1,34 +1,32 @@
 import React, {useState} from 'react';
-import {Button, Text, View, StyleSheet} from 'react-native';
-import ExStyles from './ExternalStyle'
+import {Text, View, StyleSheet, TextInput, Button} from 'react-native';
 
 const App = () => {
-  const [name, setName] = useState("Harsh Devavanshi");
+  const [data, setData] = useState('Harsh Devavanshi');
   return (
     <View>
-      {/* inline styles */}
-      <Text style = {{fontSize:20, color:"red"}}>Lorem ipsum dolor sit amet.</Text>
-      {/* Internal styles */}
-      <Text style={internalStyle.bomb}>Lorem ipsum dolor sit amet.</Text>
-      <Text style={internalStyle.bomb}>Lorem ipsum dolor sit amet.</Text>
-      <Text style={internalStyle.bomb}>Lorem ipsum dolor sit amet.</Text>
-      <Text style={ExStyles.water}>Lorem ipsum dolor sit amet.</Text>
-      <Text style = {ExStyles.clay}>Lorem ipsum dolor sit amet.</Text>
+      <Text>Handle Input</Text>
+      <Text>This is state value {data}</Text>
+      <TextInput
+        style={internalStyle.textInput}
+        placeholder="Enter your data"
+        value={data}
+        onChangeText={e => setData(e)}
+      />
+      <Button title="reset" onPress={() => setData('')} />
     </View>
   );
 };
 export default App;
-const internalStyle = StyleSheet.create(
-  {
-    bomb:{
-      color:"yellow",
-      backgroundColor: "black",
-      borderWidth: 3,
-      borderColor:"green",
-      padding: 5,
-      fontSize:20,
-      margin: 4,
-      borderRadius: 5
-    }
-  }
-)
+const internalStyle = StyleSheet.create({
+  textInput: {
+    color: 'white',
+    backgroundColor: 'black',
+    borderWidth: 2,
+    borderColor: 'blue',
+    padding: 5,
+    fontSize: 20,
+    margin: 4,
+    borderRadius: 5,
+  },
+});
