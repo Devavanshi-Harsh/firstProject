714a2ce8ddff9c90421d0310b5417d6d6b2c5dcd
= <=>
2024-02-08 23:18:19 +0530

    all three types of styles added and used.

--- App.tsx
+++ App.tsx
@@ -1,19 +1,34 @@
 import React, {useState} from 'react';
-import {Button, Text, View} from 'react-native';
-import { Colors } from 'react-native/Libraries/NewAppScreen';
+import {Button, Text, View, StyleSheet} from 'react-native';
+import ExStyles from './ExternalStyle'
 
 const App = () => {
   const [name, setName] = useState("Harsh Devavanshi");
   return (
     <View>
-      <User name = "Harsh Devavanshi" />
+      {/* inline styles */}
+      <Text style = {{fontSize:20, color:"red"}}>Lorem ipsum dolor sit amet.</Text>
+      {/* Internal styles */}
+      <Text style={internalStyle.bomb}>Lorem ipsum dolor sit amet.</Text>
+      <Text style={internalStyle.bomb}>Lorem ipsum dolor sit amet.</Text>
+      <Text style={internalStyle.bomb}>Lorem ipsum dolor sit amet.</Text>
+      <Text style={ExStyles.water}>Lorem ipsum dolor sit amet.</Text>
+      <Text style = {ExStyles.clay}>Lorem ipsum dolor sit amet.</Text>
     </View>
   );
 };
 export default App;
-
-const User= (props)=>{
-  return <View >
-    <Text style={{color:"blue", fontSize:30}}>My name is {props.name}</Text>
-  </View>
-}
+const internalStyle = StyleSheet.create(
+  {
+    bomb:{
+      color:"yellow",
+      backgroundColor: "black",
+      borderWidth: 3,
+      borderColor:"green",
+      padding: 5,
+      fontSize:20,
+      margin: 4,
+      borderRadius: 5
+    }
+  }
+)
---
+++ ExternalStyle.js
@@ -0,0 +1,18 @@
+import { StyleSheet } from "react-native";
+
+export default style = StyleSheet.create({
+    water:{
+        color:"white",
+        backgroundColor:"blue",
+        borderWidth: 10,
+        borderColor: "grey",
+        paddingTop:15,
+        paddingLeft:25,
+        margin:4
+    },
+    clay:{
+        color: "black",
+        backgroundColor:"brown",
+        borderColor: "grey"
+    }
+})
