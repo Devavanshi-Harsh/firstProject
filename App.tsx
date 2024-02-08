92f0c28039ecbf4d42da9ee2e04609d4bc12fe70
= <=>
2024-02-08 23:57:36 +0530

    form fields created and used

--- App.tsx
+++ App.tsx
@@ -2,31 +2,58 @@ import React, {useState} from 'react';
 import {Text, View, StyleSheet, TextInput, Button} from 'react-native';
 
 const App = () => {
-  const [data, setData] = useState('Harsh Devavanshi');
+  const [name, setName] = useState('');
+  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState('');
+  const [display, setDisplay] = useState(false);
+  function resetHandler() {
+    setName('');
+    setPassword('');
+    setEmail('');
+    setDisplay(false);
+  }
   return (
     <View>
-      <Text>Handle Input</Text>
-      <Text>This is state value {data}</Text>
       <TextInput
         style={internalStyle.textInput}
-        placeholder="Enter your data"
-        value={data}
-        onChangeText={e => setData(e)}
+        placeholder="Enter your name"
+        value={name}
+        onChangeText={e => setName(e)}
       />
-      <Button title="reset" onPress={() => setData('')} />
+      <TextInput
+        style={internalStyle.textInput}
+        placeholder="Enter password"
+        secureTextEntry={true}
+        value={password}
+        onChangeText={e => setPassword(e)}
+      />
+      <TextInput
+        style={internalStyle.textInput}
+        placeholder="Enter your email address"
+        value={email}
+        onChangeText={e => setEmail(e)}
+      />
+      <Button color={'green'} title="Print" onPress={() => setDisplay(true)} />
+      <Button color={'red'} title="reset" onPress={resetHandler} />
+      {display && (
+        <View>
+          <Text>Name : {name}</Text>
+          <Text>Password : {password}</Text>
+          <Text>Email Address : {email}</Text>
+        </View>
+      )}
     </View>
   );
 };
 export default App;
 const internalStyle = StyleSheet.create({
   textInput: {
-    color: 'white',
-    backgroundColor: 'black',
+    color: 'blue',
     borderWidth: 2,
     borderColor: 'blue',
-    padding: 5,
+    padding: 15,
     fontSize: 20,
-    margin: 4,
+    margin: 6,
     borderRadius: 5,
   },
 });
