b8a70ba396a8bbb753ce55219e3f17b99db278e2
= <=>
2024-02-12 17:17:27 +0530

    props and state are shown in student component

--- App.tsx
+++ App.tsx
@@ -1,13 +1,14 @@
 import {Component} from 'react';
 import {Text, View} from 'react-native';
 import User from './components/User';
+import Student from './components/Student';
 
 class App extends Component {
   render() {
     return (
       <View>
         <Text>This is App Class Component</Text>
-        <User />
+        <Student name = "Rahul" />
       </View>
     );
   }
