05f3b1bdc8a8740f5d9adc63b35c36eb86600c5b
Harsh Devavanshi <hdevavan@gmail.com>
2024-02-13 09:36:22 +0530

    RadioButtons

--- App.tsx
+++ App.tsx
@@ -1,47 +1,57 @@
 import React, {useEffect, useState} from 'react';
-import {Button, StyleSheet, Text, View} from 'react-native';
+import {Button, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 
 export default App = () => {
-  const [display, setDisplay] = useState(true);
+  const [check, setCheck] = useState(0);
   return (
-    <View style={style.boxes}>
-      <View style={style.box1}>
-        <Text style={{backgroundColor: 'orange', margin: 10, flex: 1}}></Text>
-        <Text style={{backgroundColor: 'grey', margin: 10, flex: 1}}></Text>
-        <Text style={{backgroundColor: 'skyblue', margin: 10, flex: 1}}></Text>
-      </View>
-      <View style={style.box2}>
-        <Text>Box2</Text>
-      </View>
-      <View style={style.box3}>
-        <Text>Box3</Text>
-      </View>
-      <View style={style.box4}>
-        <Text>Box4</Text>
-      </View>
+    <View style={style.main}>
+      <TouchableOpacity onPress={() => setCheck(1)}>
+        <View style={style.radioWrapper}>
+          <View style={style.radio}>
+            {check === 1 && <View style={style.radioBg}></View>}
+          </View>
+          <View>
+            <Text style={style.radioText}>Radio 1</Text>
+          </View>
+        </View>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => setCheck(2)}>
+        <View style={style.radioWrapper}>
+          <View style={style.radio}>
+            {check === 2 && <View style={style.radioBg}></View>}
+          </View>
+          <View>
+            <Text style={style.radioText}>Radio 1</Text>
+          </View>
+        </View>
+      </TouchableOpacity>
     </View>
   );
 };
 const style = StyleSheet.create({
-  boxes: {
+  main: {
     flex: 1,
-    backgroundColor: 'grey',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
-  box1: {
-    flex: 2,
-    backgroundColor: 'red',
-    flexDirection: 'row',
+  radioWrapper: {margin: 10, flexDirection: 'row', alignItems: 'center'},
+  radio: {
+    borderWidth: 1,
+    borderColor: 'black',
+    width: 50,
+    height: 50,
+    borderRadius: 50,
+    margin: 10,
   },
-  box2: {
-    flex: 1,
-    backgroundColor: 'green',
+  radioBg: {
+    backgroundColor: 'black',
+    width: 40,
+    height: 40,
+    borderRadius: 50,
+    margin: 4,
   },
-  box3: {
-    flex: 1,
-    backgroundColor: 'yellow',
-  },
-  box4: {
-    flex: 1,
-    backgroundColor: 'blue',
+  radioText: {
+    padding: 10,
+    fontSize: 20,
   },
 });
