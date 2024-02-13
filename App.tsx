0418b8aebec793e9d7b2378029d694f9d70a31ad
Harsh Devavanshi <hdevavan@gmail.com>
2024-02-13 08:59:44 +0530

    TouchableHighlight

--- App.tsx
+++ App.tsx
@@ -1,47 +1,56 @@
 import React, {useEffect, useState} from 'react';
-import {Button, StyleSheet, Text, View} from 'react-native';
+import {Button, StyleSheet, Text, TouchableHighlight, View} from 'react-native';
 
 export default App = () => {
   const [display, setDisplay] = useState(true);
   return (
-    <View style={style.boxes}>
-      <View style={style.box1}>
-        <Text style={{backgroundColor: 'orange', margin: 10, flex: 1}}></Text>
-        <Text style={{backgroundColor: 'grey', margin: 10, flex: 1}}></Text>
-        <Text style={{backgroundColor: 'skyblue', margin: 10, flex: 1}}></Text>
-      </View>
-      <View style={style.box2}>
-        <Text>Box2</Text>
-      </View>
-      <View style={style.box3}>
-        <Text>Box3</Text>
-      </View>
-      <View style={style.box4}>
-        <Text>Box4</Text>
-      </View>
+    <View style={{flex: 1}}>
+      <TouchableHighlight>
+        <Text style={[style.button, style.success]}>Success</Text>
+      </TouchableHighlight>
+      <TouchableHighlight>
+        <Text style={[style.button, style.primary]}>Primary</Text>
+      </TouchableHighlight>
+      <TouchableHighlight>
+        <Text style={[style.button, style.warning]}>Warning</Text>
+      </TouchableHighlight>
+      <TouchableHighlight>
+        <Text style={[style.button, style.error]}>Error</Text>
+      </TouchableHighlight>
+      <TouchableHighlight>
+        <Text style={style.button}>Button</Text>
+      </TouchableHighlight>
+      <TouchableHighlight>
+        <Text style={[style.button, style.soothing]}>Soothing</Text>
+      </TouchableHighlight>
     </View>
   );
 };
 const style = StyleSheet.create({
-  boxes: {
-    flex: 1,
+  button: {
+    fontSize: 20,
+    margin: 10,
     backgroundColor: 'grey',
+    padding: 10,
+    shadowColor: 'green',
+    elevation: 10,
+    borderRadius: 10,
+    textAlign: 'center',
+    color: 'white',
   },
-  box1: {
-    flex: 2,
-    backgroundColor: 'red',
-    flexDirection: 'row',
-  },
-  box2: {
-    flex: 1,
+  success: {
     backgroundColor: 'green',
   },
-  box3: {
-    flex: 1,
+  primary: {
+    backgroundColor: 'blue',
+  },
+  warning: {
     backgroundColor: 'yellow',
   },
-  box4: {
-    flex: 1,
-    backgroundColor: 'blue',
+  error: {
+    backgroundColor: 'red',
+  },
+  soothing: {
+    backgroundColor: 'orange',
   },
 });
