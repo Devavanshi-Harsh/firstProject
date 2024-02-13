f1154bc29d19ab9d1571148de2ad09dfdf4a1f22
Harsh Devavanshi <hdevavan@gmail.com>
2024-02-13 19:26:49 +0530

    Activity Indicators

--- App.tsx
+++ App.tsx
@@ -1,50 +1,26 @@
-import React, {useEffect, useState} from 'react';
-import {Button, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import React, {useState} from 'react';
+import {ActivityIndicator, Button, StyleSheet, View} from 'react-native';
 
 export default App = () => {
-  const skills = [
-    {
-      id: 0,
-      skill: 'Kotlin',
-    },
-    {
-      id: 1,
-      skill: 'Java',
-    },
-    {
-      id: 2,
-      skill: 'Ruby',
-    },
-    {
-      id: 3,
-      skill: 'C++',
-    },
-    {
-      id: 4,
-      skill: 'Python',
-    },
-    {
-      id: 5,
-      skill: 'JavaScript',
-    },
-  ];
-  const [check, setCheck] = useState(0);
+  const [check, setCheck] = useState(true);
   return (
     <View style={style.main}>
-      {skills.map((item, index) => {
-        return (
-          <TouchableOpacity onPress={() => setCheck(index)} key={index}>
-            <View style={style.radioWrapper}>
-              <View style={style.radio}>
-                {check === item.id && <View style={style.radioBg}></View>}
-              </View>
-              <View>
-                <Text style={style.radioText}>{item.skill}</Text>
-              </View>
-            </View>
-          </TouchableOpacity>
-        );
-      })}
+      <Button
+        title={`${check ? 'Hide' : 'Show'} Indicators`}
+        color={check ? 'red' : 'green'}
+        onPress={() => setCheck(!check)}
+      />
+      <ActivityIndicator size={'large'} color={'red'}></ActivityIndicator>
+      {check && (
+        <ActivityIndicator
+          size={200}
+          color={'green'}
+          animating={true}></ActivityIndicator>
+      )}
+      <ActivityIndicator size={'small'} color={'blue'}></ActivityIndicator>
+      {check && (
+        <ActivityIndicator size={100} color={'yellow'}></ActivityIndicator>
+      )}
     </View>
   );
 };
@@ -53,25 +29,5 @@ const style = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-  },
-  radioWrapper: {margin: 10, flexDirection: 'row', alignItems: 'center'},
-  radio: {
-    borderWidth: 1,
-    borderColor: 'black',
-    width: 50,
-    height: 50,
-    borderRadius: 50,
-    margin: 10,
-  },
-  radioBg: {
-    backgroundColor: 'black',
-    width: 40,
-    height: 40,
-    borderRadius: 50,
-    margin: 4,
-  },
-  radioText: {
-    padding: 10,
-    fontSize: 20,
   },
 });
