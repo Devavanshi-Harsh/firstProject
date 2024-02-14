22b93d22d991f03629e54e692fa4737d4086551a
Harsh Devavanshi <hdevavan@gmail.com>
2024-02-14 17:50:18 +0530

    Status Bar

--- App.tsx
+++ App.tsx
@@ -1,18 +1,20 @@
-import React from 'react';
-import {Text, View, Pressable} from 'react-native';
+import React, {useState} from 'react';
+import {View, StatusBar, Button} from 'react-native';
 import {style} from './style';
 
 export default App = () => {
+  const [check, setCheck] = useState(false);
   return (
     <View style={style.main}>
-      <Pressable
-        style={style.pressableContainer}
-        onPress={() => console.warn('Normal Press')}
-        onLongPress={() => console.warn('Long Press')}
-        onPressIn={() => console.warn('Press In')}
-        onPressOut={() => console.warn('Press Out')}>
-        <Text style={style.pressableText}>Pressable Text</Text>
-      </Pressable>
+      <StatusBar
+        backgroundColor={'orange'}
+        barStyle={'dark-content'}
+        hidden={check}
+      />
+      <Button
+        title={`${check ? 'show' : 'hide'} Status Bar`}
+        onPress={() => setCheck(!check)}
+      />
     </View>
   );
 };
