478217e7ecb8e9759960201fc415b4ff380af0be
Harsh Devavanshi <hdevavan@gmail.com>
2024-02-14 17:23:30 +0530

    Modals

--- App.tsx
+++ App.tsx
@@ -1,33 +1,22 @@
 import React, {useState} from 'react';
-import {ActivityIndicator, Button, StyleSheet, View} from 'react-native';
+import {Text, Button, Modal, StyleSheet, View} from 'react-native';
+import {style} from './style';
 
 export default App = () => {
   const [check, setCheck] = useState(true);
   return (
     <View style={style.main}>
-      <Button
-        title={`${check ? 'Hide' : 'Show'} Indicators`}
-        color={check ? 'red' : 'green'}
-        onPress={() => setCheck(!check)}
-      />
-      <ActivityIndicator size={'large'} color={'red'}></ActivityIndicator>
-      {check && (
-        <ActivityIndicator
-          size={200}
-          color={'green'}
-          animating={true}></ActivityIndicator>
-      )}
-      <ActivityIndicator size={'small'} color={'blue'}></ActivityIndicator>
-      {check && (
-        <ActivityIndicator size={100} color={'yellow'}></ActivityIndicator>
-      )}
+      <Modal transparent={true} visible={check} animationType="fade">
+        <View style={style.modalContainer}>
+          <View style={style.textContainer}>
+            <Text style={style.modalText}>This is the data to show</Text>
+            <Button title="close modal" onPress={() => setCheck(false)} />
+          </View>
+        </View>
+      </Modal>
+      <View>
+        <Button title="Show Modal" onPress={() => setCheck(true)} />
+      </View>
     </View>
   );
 };
-const style = StyleSheet.create({
-  main: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
