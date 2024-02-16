e73824fd32b96b952a5dd57ed11b40a7157fe403
Harsh Devavanshi <hdevavan@gmail.com>
2024-02-16 21:56:08 +0530

    WebView Component in React-Native-WebView

--- App.tsx
+++ App.tsx
@@ -1,25 +1,12 @@
-import React, {useState} from 'react';
-import {View, Text, Platform, StyleSheet} from 'react-native';
-import {json} from 'stream/consumers';
+import React from 'react';
+import WebView from 'react-native-webview';
 
 export default App = () => {
-  const [check, setCheck] = useState(false);
   return (
-    <View style={style.main}>
-      <Text>OS is : {Platform.OS} \n</Text>
-      <Text style={style.platformText}>
-        This font will be orange if IOS device otherwise Red for Android Device
-      </Text>
-      <Text>{JSON.stringify(Platform)}</Text>
-    </View>
+    <WebView
+      source={{
+        uri: 'https://stackoverflow.com/questions/883977/display-json-as-html',
+      }}
+    />
   );
 };
-export const style = StyleSheet.create({
-  main: {
-    margin: 10,
-  },
-  platformText: {
-    color: Platform.OS == 'android' ? 'red' : 'green',
-    fontSize: 15,
-  },
-});
