9344a1008bd7dd141cd1e898eb306a794f0586f2
Harsh Devavanshi <hdevavan@gmail.com>
2024-02-16 22:14:24 +0530

    Custom Modal Dialogue Box

--- App.tsx
+++ App.tsx
@@ -1,12 +1,20 @@
-import React from 'react';
-import WebView from 'react-native-webview';
-
+import React, {useState} from 'react';
+import {Button, StyleSheet, Text, View} from 'react-native';
+import style from './style';
 export default App = () => {
+  const [display, setDisplay] = useState(false);
+
   return (
-    <WebView
-      source={{
-        uri: 'https://stackoverflow.com/questions/883977/display-json-as-html',
-      }}
-    />
+    <View style={style.container}>
+      {display && (
+        <View style={style.model}>
+          <View style={style.body}>
+            <Text style={style.text}>This is custom Modal</Text>
+            <Button title="Close It" onPress={() => setDisplay(false)} />
+          </View>
+        </View>
+      )}
+      <Button title="Open Custom Modal" onPress={() => setDisplay(true)} />
+    </View>
   );
 };
