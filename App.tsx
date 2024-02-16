5cc91913c3b475d8b82109b9fa79ff6abcef6906
Harsh Devavanshi <hdevavan@gmail.com>
2024-02-16 17:34:13 +0530

    Platform

--- App.tsx
+++ App.tsx
@@ -1,20 +1,24 @@
 import React, {useState} from 'react';
-import {View, StatusBar, Button} from 'react-native';
-import {style} from './style';
+import {View, Text, Platform, StyleSheet} from 'react-native';
+import {json} from 'stream/consumers';
 
 export default App = () => {
   const [check, setCheck] = useState(false);
   return (
     <View style={style.main}>
-      <StatusBar
-        backgroundColor={'orange'}
-        barStyle={'dark-content'}
-        hidden={check}
-      />
-      <Button
-        title={`${check ? 'show' : 'hide'} Status Bar`}
-        onPress={() => setCheck(!check)}
-      />
+      <Text>OS is : {Platform.OS} \n</Text>
+      <Text style={style.platformText}>
+        This font will be orange if IOS device otherwise Red for Android Device
+      </Text>
     </View>
   );
 };
+export const style = StyleSheet.create({
+  main: {
+    margin: 10,
+  },
+  platformText: {
+    color: Platform.OS == 'android' ? 'red' : 'green',
+    fontSize: 15,
+  },
+});
