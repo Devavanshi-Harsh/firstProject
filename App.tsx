b4d66f8e07c5991df246f57042538be6cb8d791e
Harsh Devavanshi <hdevavan@gmail.com>
2024-02-17 00:03:34 +0530

    StackNavigation

--- App.tsx
+++ App.tsx
@@ -1,20 +1,35 @@
-import React, {useState} from 'react';
-import {Button, StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import {Button, Text, View} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import style from './style';
+const Stack = createNativeStackNavigator();
 export default App = () => {
-  const [display, setDisplay] = useState(false);
-
   return (
-    <View style={style.container}>
-      {display && (
-        <View style={style.model}>
-          <View style={style.body}>
-            <Text style={style.text}>This is custom Modal</Text>
-            <Button title="Close It" onPress={() => setDisplay(false)} />
-          </View>
-        </View>
-      )}
-      <Button title="Open Custom Modal" onPress={() => setDisplay(true)} />
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Log In" component={Login} />
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+const Home = () => {
+  return (
+    <View style={style.box}>
+      <Text style={style.text}>This is Home Component</Text>
+    </View>
+  );
+};
+const Login = props => {
+  return (
+    <View style={style.box}>
+      <Text style={style.text}>This is Login Component</Text>
+      <Button
+        title="Go to home"
+        onPress={() => props.navigation.navigate('Home')}
+      />
     </View>
   );
 };
