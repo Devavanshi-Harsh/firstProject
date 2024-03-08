006038d2d6637c560e3a44e79e34b5827aa74676
Harsh Devavanshi <hdevavan@gmail.com>
2024-03-08 17:33:43 +0530

    TabNavigation implemented on basics

--- App.tsx
+++ App.tsx
@@ -1,37 +1,16 @@
 import React from 'react';
-import {Button, Pressable, StyleSheet, Text, View} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import Home from './components/Home';
-import Cart from './components/Cart';
 import Login from './components/Login';
-import style from './components/Style';
-const Stack = createNativeStackNavigator();
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+const Tab = createBottomTabNavigator();
 export default App = () => {
-  function clickHandler(i: string) {
-    console.warn('Click Handler', i);
-  }
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="Log In"
-          component={Login}
-          options={{
-            headerTitle: () => (
-              <Button onPress={() => clickHandler('title')} title="Title" />
-            ),
-            headerLeft: () => (
-              <Button onPress={() => clickHandler('left')} title="Left" />
-            ),
-            headerRight: () => <Cart clickHandler={clickHandler} />,
-            headerStyle: {backgroundColor: 'blue'},
-            headerTintColor: 'white',
-            headerTitleStyle: {fontSize: 30},
-          }}
-        />
-        <Stack.Screen name="Home" component={Home} />
-      </Stack.Navigator>
+      <Tab.Navigator>
+        <Tab.Screen name="Log In" component={Login} />
+        <Tab.Screen name="Home" component={Home} />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 };
