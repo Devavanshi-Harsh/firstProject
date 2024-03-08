d8ae01525615daa2b30f8dc29ad647e4b948af25
Harsh Devavanshi <hdevavan@gmail.com>
2024-03-08 18:17:23 +0530

    simple api called

--- App.tsx
+++ App.tsx
@@ -1,16 +1,30 @@
-import React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
-import Home from './components/Home';
-import Login from './components/Login';
+import React, {useEffect, useState} from 'react';
 import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs';
-const Tab = createMaterialTopTabNavigator();
+import {FlatList, SectionList, Text, View} from 'react-native';
 export default App = () => {
+  const [data, setData] = useState('');
+
+  const fetchAPIData = async () => {
+    const url = 'https://jsonplaceholder.typicode.com/posts/1';
+    let result = await fetch(url);
+    result = await result.json();
+    setData(result);
+  };
+
+  useEffect(() => {
+    fetchAPIData();
+  }, []);
   return (
-    <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="Log In" component={Login} />
-        <Tab.Screen name="Home" component={Home} />
-      </Tab.Navigator>
-    </NavigationContainer>
+    <View>
+      <Text>API Call</Text>
+      {data ? (
+        <View>
+          <Text style={{fontSize: 20}}>User ID: {data.userId}</Text>
+          <Text style={{fontSize: 20}}>ID: {data.id}</Text>
+          <Text style={{fontSize: 20}}>Title: {data.title}</Text>
+          <Text style={{fontSize: 20}}>Body : {data.body}</Text>
+        </View>
+      ) : null}
+    </View>
   );
 };
