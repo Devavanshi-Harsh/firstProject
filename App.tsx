0fee6827e524d19669afd59939ab091b88e381bb
Harsh Devavanshi <hdevavan@gmail.com>
2024-03-08 19:14:16 +0530

    Loading feature added and notes updated.

--- App.tsx
+++ App.tsx
@@ -1,6 +1,5 @@
 import React, {useEffect, useState} from 'react';
-import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs';
-import {FlatList, ScrollView, SectionList, Text, View} from 'react-native';
+import {ScrollView, Text, View} from 'react-native';
 export default App = () => {
   const [data, setData] = useState([]);
 
@@ -17,6 +16,9 @@ export default App = () => {
   return (
     <ScrollView>
       <Text>API Call</Text>
+      {data.length == 0 && (
+        <Text style={{textAlign: 'center', fontSize: 40}}>Loading...</Text>
+      )}
       {data
         ? data.map((item, index) => (
             <View key={index}>
